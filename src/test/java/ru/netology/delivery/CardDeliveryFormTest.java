30aecdc82238a2834685d8290f3faa9196995a55
Andrey Martyn <martynandrey@rambler.ru>
2022-06-10 00:01:20 +0600

    Allure implemented

--- src/test/java/ru/netology/delivery/CardDeliveryFormTest.java
+++ src/test/java/ru/netology/delivery/CardDeliveryFormTest.java
@@ -2,13 +2,28 @@ package ru.netology.delivery;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryFormTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @Test
     public void shouldSendOrderForm() {
